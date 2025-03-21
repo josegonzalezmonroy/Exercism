36aee6076e078427daa05bc60fab7d295df3a8a0
Jose Gonzalez <josegonzalezmonroy@gmail.com>
2025-03-21 15:16:24 -0300

    adiciona implementação da classe ResistorColor, configuração do Gradle e testes

---
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -0,0 +1,18 @@
+class ResistorColor {
+    private static final String[] COLORS = {
+        "black", "brown", "red", "orange", "yellow", 
+        "green", "blue", "violet", "grey", "white"
+    };
+
+    int colorCode(String color) {
+        for (int i = 0; i < COLORS.length; i++) 
+            if (COLORS[i].equals(color))
+                return i;
+        
+        throw new IllegalArgumentException("Cor inválida: " + color);
+    }
+
+    String[] colors() {
+        return COLORS;
+    }
+}
